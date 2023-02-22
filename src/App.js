3698cd72b9299aa2203f7d82f3c460295d06f540
Deepak Singh <93373642+DrangonPath@users.noreply.github.com>
2023-02-22 21:43:27 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+<div>
+      <header>
+        <nav>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+        <h1>Reasons I'm excited to learn React</h1>
+      </header>
+      <ol>
+        <li>React is a popular and in-demand skill</li>
+        <li>React allows for fast and efficient development of complex user interfaces</li>
+        <li>React has a large and active community with plenty of resources and support</li>
+      </ol>
+      <footer>© 2023  development. All rights reserved.</footer>
+    </div>
+  );
+}
+
+export default App;
